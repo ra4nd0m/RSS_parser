9280ced8993d23a59cdd3ea121c231d0074904ee
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-07-13 20:17:56 +0300

    Comments for RSS_parse.js

--- src/RSS_parse.js
+++ src/RSS_parse.js
@@ -3,14 +3,15 @@ let parser = new Parser();
 
 async function getRSS(url) {
     let feed;
+    //try using http method if fails run puppeteer
     try {
         feed = await parser.parseURL(url);
     } catch (err) {
         feed = await getRSS_Stealth(url);
-    }
+    } //parse recived feed 
     return await parseRSS(feed);
 };
-
+//site scrapping using puppeteer
 async function getRSS_Stealth(url) {
     const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({ headless: 'new' });
@@ -22,12 +23,12 @@ async function getRSS_Stealth(url) {
     await browser.close();
     return await parser.parseString(string);
 }
-
+//parse data using rss-parser
 async function parseRSS(feed) {
     let obj = [];
     console.log(feed.title);
     const today = new Date().toISOString().split('T')[0];
-
+    //make sure the date of articles is today
     const todayArticles = feed.items.filter(item => item.isoDate.startsWith(today));
     todayArticles.forEach(item => {
         obj.push({

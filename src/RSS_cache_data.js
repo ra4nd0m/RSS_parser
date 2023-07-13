b7ba5b0d2c328d2ddf0d317379ee75a2a493ea62
Egor Kosyakov <58385866+ra4nd0m@users.noreply.github.com>
2023-07-13 20:20:04 +0300

    commented RSS_cache_data.js

--- src/RSS_cache_data.js
+++ src/RSS_cache_data.js
@@ -4,23 +4,29 @@ const db_url = process.env.DB_URL;
 const db = process.env.DB;
 
 async function cache_data(data,source_site) {
+    //connect to mongodb
     console.log(db_url,db);
     const client = new MongoClient(db_url);
     await client.connect().then(() => {
         console.log('Connected!');
     });
     let obj = [];
+    //get the requierd collection
     const collection = client.db(db).collection(source_site);
+    //check if there any duplicates
     for (const item of data) {
         const result = await collection.find({ link: item.link }).toArray();
+        //if no dupes found => this is new article so we push it in obj to return
         if (result.length === 0) {
             console.log("New article: ",item.title);
             await collection.insertOne(item);
             obj.push(item);
         }else{
+            //else do nothing
             console.log("FOUND! ",item.title);
         }
     }
+    //close connection and return data
     await client.close();
     return obj;
 }
